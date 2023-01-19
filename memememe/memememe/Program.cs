82eb0599661bf8b5dfeb775ae647327d86b8ab53
Дарья Лавренюк <114358972+fatfreakdasha@users.noreply.github.com>
2023-01-19 09:48:18 +0300

    Add files via upload

---
+++ memememe/memememe/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace memememe
+{
+    class Program
+    {
+        static bool IsPerfectSquare(int x)
+        {
+            return Math.Sqrt(x) % 1 == 0;
+        }
+
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("введите число:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int countNumbers = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsPerfectSquare(i))
+                    ++countNumbers;
+
+            }
+            Console.Write("количество чисел,являющиеся полными квадратами: {0}", countNumbers);
+            Console.ReadLine();
+        }
+    }
+}
